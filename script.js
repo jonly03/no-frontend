47661af3badcff42d08ec26e161829bd5851a4bf
Nelly Sugu <jonly03@gmail.com>
2021-12-08 13:49:04 -0500

    connected to API

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-// const API_URL = "";
+const API_URL = "https://no-backend.herokuapp.com/";
 
-// fetch(API_URL)
-//   .then((res) => res.json())
-//   .then((data) => {
-const container = document.getElementById("data_container");
-const data = [
-  { title: "title 1" },
-  { title: "title 2" },
-  { title: "title 3" },
-  { title: "title 3" },
-];
-for (const elt of data) {
-  const newP = document.createElement("p");
-  newP.innerText = elt.title;
+fetch(API_URL)
+  .then((res) => res.json())
+  .then((data) => {
+    const container = document.getElementById("data_container");
 
-  container.appendChild(newP);
-}
-//   });
+    for (const elt of data) {
+      const newP = document.createElement("p");
+      newP.innerText = elt.title;
+
+      container.appendChild(newP);
+    }
+  });
